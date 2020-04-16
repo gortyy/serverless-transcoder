4f62ae3b084be1c47b34cba0cf544e2b4e88066b
Dawid <35574037+gortyy@users.noreply.github.com>
2020-04-16 09:30:27 +0200

    Lambdas initial (#2)
    * Initial version of lambda functions
    
    * Handle specific exception

---
+++ lambdas/transcode_video/transcode_video.py
@@ -0,0 +1,39 @@
+import json
+import urllib
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+def handler(event, context):
+    key = event["Records"][0]["s3"]["object"]["key"]
+    source_key = urllib.parse.unquote_plus(key)
+    output_key = source_key.split(".")[0]
+    print(f"key: ", key, source_key, output_key)
+
+    transcoder_params = {
+        "PipelineId": "1586883367960-0uo4tl",
+        "OutputKeyPrefix": output_key + "/",
+        "Input": {"Key": source_key},
+        "Outputs": [
+            {
+                "Key": f"{output_key}-1080p.mp4",
+                "PresetId": "1351620000001-000001",
+            },
+            {
+                "Key": f"{output_key}-720p.mp4",
+                "PresetId": "1351620000001-000010",
+            },
+            {
+                "Key": f"{output_key}-web-720p.mp4",
+                "PresetId": "1351620000001-100070",
+            },
+        ],
+    }
+
+    elastic_transcoder = boto3.client("elastictranscoder", "us-east-1")
+    try:
+        response = elastic_transcoder.create_job(**transcoder_params)
+        return json.dumps({"message": response})
+    except ClientError as exc:
+        return json.dumps({"error": str(exc)})
