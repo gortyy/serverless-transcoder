d77594a01c72ccf4d5edf468f2c3d3ea6e156c2e
gortyy <dwsmiech@gmail.com>
2020-04-16 09:24:24 +0200

    Initial version of lambda functions

---
+++ lambdas/set_permissions/set_permissions.py
@@ -0,0 +1,20 @@
+import json
+import urllib
+
+import boto3
+from botocore.exceptions import ClientError
+
+
+def handler(event, context):
+    message = event["Records"][0]
+    source_bucket = message["s3"]["bucket"]["name"]
+    source_key = message["s3"]["object"]["key"].replace("+", " ")
+
+    s3 = boto3.resource("s3")
+    try:
+        response = (
+            s3.Object(source_bucket, source_key).Acl().put(ACL="public-read")
+        )
+        return json.dumps({"message": response})
+    except ClientError as exc:
+        return json.dumps({"error": str(exc)})
