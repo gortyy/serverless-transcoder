145d051ca0a53c8ae7a4ba25d4af8e3a5642bbce
gortyy <dwsmiech@gmail.com>
2020-04-20 09:07:57 +0200

    Prepare for unit tests

--- tests/test_set_permissions.py
+++ tests/test_set_permissions.py
@@ -1,5 +1,9 @@
 import lambdas.set_permissions.set_permissions as sp
 
 
+def test_parse_event(set_permissions_event):
+    pass
+
+
 def test_handler():
     print(sp.handler)
--- tests/test_transcode_video.py
+++ tests/test_transcode_video.py
@@ -1,5 +1,9 @@
 import lambdas.transcode_video.transcode_video as tv
 
 
+def test_parse_event(transcode_video_event):
+    pass
+
+
 def test_handler():
     print(tv.handler)
